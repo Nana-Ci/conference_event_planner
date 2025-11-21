0e1fb642028fd06049746448587398a47d048959
Nana-Ci <amatsuda@kf7.so-net.ne.jp>
2025-11-21 07:20:04 -0500

    bug fixed, code cleaned

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-import avReducer from './avSlice'
-import mealsReducer from './meal'
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
